ac83c7dc83f8ce25c808e81fbed7cd1ccfcd150e
Purrbiscuit <spurr@articulate.com>
2021-02-09 17:10:53 -0500

    add jest test suite

--- tests/jest/test.js
+++ tests/jest/test.js
@@ -1,3 +1,26 @@
-it('works', () => {
-  
+const { orderTotal } = require('../../src/orders')
+
+const {
+  freeShippingOrderStub,
+  multipleQuantityOrderStub,
+  noQuantityOrderStub,
+  paidShippingOrderStub
+} = require('../stubs')
+
+describe('Order Totals', () => {
+  it('Free shipping', () => {
+    expect(orderTotal(freeShippingOrderStub)).toBe(808)
+  })
+
+  it('Multiple Quantities', () => {
+    expect(orderTotal(multipleQuantityOrderStub)).toBe(200)
+  })
+
+  it('No Quantity', () => {
+    expect(orderTotal(noQuantityOrderStub)).toBe(28)
+  })
+
+  it('Paid shipping', () => {
+    expect(orderTotal(paidShippingOrderStub)).toBe(48)
+  })
 })
